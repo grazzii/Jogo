5b1b64c474a5f55a4c4e5647fe13c9016abae169
Luis Felipe <luisfelipesdn12@gmail.com>
2025-04-30 20:26:12 -0300

    ✨ feat: Update pygame setup and implement position cycling

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((600, 600))
 clock = pygame.time.Clock()
 running = True
+
+positions = [
+    (125, 300),
+    (300, 125),
+    (475, 300),
+    (300, 475)
+]
+
+pos = 0
 
 while running:
     # poll for events
@@ -14,10 +23,26 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                pos += 1
+                if pos > len(positions) - 1:
+                    pos = 0
+                    
+    # every 1 seconds
+    if pygame.time.get_ticks() % (60 * 1) == 0:
+        pos += 1
+        if pos > len(positions) - 1:
+            pos = 0
 
-    # RENDER YOUR GAME HERE
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("#FFD166")
+
+    pos_circle = positions[pos]
+
+    pygame.draw.circle(screen, "#FB6286", (300, 300), 200)
+    pygame.draw.circle(screen, "#FFD166", (300, 300), 150)
+    pygame.draw.circle(screen, "#073B4C", pos_circle, 25)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
